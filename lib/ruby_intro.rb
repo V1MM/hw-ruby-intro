1ef4775bcfcae1fb873533fa458a8da7b09f5f9d
SomeBody <nene.anantakul@gmail.com>
2024-12-21 11:36:35 +0000

    Part 1&2

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,54 @@
 
 def sum(arr)
   # YOUR CODE HERE
+  arr.sum
 end
 
-def max_2_sum(arr)
-  # YOUR CODE HERE
+def max_2_sum(array)
+  return 0 if array.empty?          
+  return array.first if array.size == 1
+
+  array.max(2).sum 
 end
 
-def sum_to_n?(arr, n)
-  # YOUR CODE HERE
+
+def sum_to_n?(array, n)
+  return false if array.size < 2 
+
+  complements = {}
+  array.each do |num|
+    return true if complements[num] 
+    complements[n - num] = true
+  end
+
+  false 
 end
+
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant?(s)
-  # YOUR CODE HERE
+  return false if s.empty? 
+  first_char = s[0].downcase 
+
+  first_char =~ /[a-z]/ && first_char !~ /[aeiou]/ ? true : false
+
 end
 
+
 def binary_multiple_of_4?(s)
-  # YOUR CODE HERE
+  return false if s.empty? || s.match(/[^01]/)
+  s[-2..-1] == '00' || s == '0'
 end
+
 
 # Part 3
 
 class BookInStock
   # YOUR CODE HERE
+  
 end
